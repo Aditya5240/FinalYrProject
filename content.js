3f75c058e6bb5e9f8a2a1385deac27e0efb529ef
Aditya5240 <adityagwl2004@gmail.com>
2026-09-23 18:33:47 +0530

    feat: detect Gmail compose box appearance using MutationObserver

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 // console.log("Gmail AI Reply: content script loaded");
 // learning-1 : ctrl + shift + I ,open console
 
-window.addEventListener("load", () => {
-    console.log("Gmail page fully load");
-    console.log("Page title is: ", document.title);
-});
+// window.addEventListener("load", () => {
+//     console.log("Gmail page fully load");
+//     console.log("Page title is: ", document.title);
+// });
 
 // learning-2 : mutation observer is a built in browser API that watches a 
 //              part of the DOM and fires a callback everytime something changes
@@ -13,7 +13,7 @@ window.addEventListener("load", () => {
 
 // let count = 0;
 
-// const observerr = new MutationObserver(() => {
+// const observer = new MutationObserver(() => {
 //     count++;
 //     console.log("DOM changed, mutation #", count);
 //     if(count >= 5) {
@@ -23,3 +23,25 @@ window.addEventListener("load", () => {
 // });
 
 // observer.observe(document.body, { childList: true, subtree: true });
+
+// window.addEventListener("load", () => {
+//     const composeBoxes = document.querySelectorAll('div[aria-label="Message Body"][role="textbox"]');
+//     console.log("Found", composeBoxes.length, "compose boxes");
+
+//     composeBoxes.forEach((box) => {
+//         console.log(box);
+//     });
+// });
+
+function scanForComposeBoxes() {
+  const composeBoxes = document.querySelectorAll('div[aria-label="Message Body"][role="textbox"]');
+  console.log("Found", composeBoxes.length, "compose boxes");
+}
+
+const observer = new MutationObserver(() => {
+  scanForComposeBoxes();
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+scanForComposeBoxes();
