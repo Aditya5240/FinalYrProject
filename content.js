8538084125de080dd0aa9eedbd5d02055ca5c0bf
Aditya5240 <adityagwl2004@gmail.com>
2026-09-23 18:10:28 +0530

    wip: experiment with MutationObserver for Gmail DOM changes

--- content.js
+++ content.js
@@ -1,2 +1,25 @@
-console.log("Gmail AI Reply: content script loaded");
-// ctrl + shift + I ,open console
+// console.log("Gmail AI Reply: content script loaded");
+// learning-1 : ctrl + shift + I ,open console
+
+window.addEventListener("load", () => {
+    console.log("Gmail page fully load");
+    console.log("Page title is: ", document.title);
+});
+
+// learning-2 : mutation observer is a built in browser API that watches a 
+//              part of the DOM and fires a callback everytime something changes
+//              inside it.   
+
+
+// let count = 0;
+
+// const observerr = new MutationObserver(() => {
+//     count++;
+//     console.log("DOM changed, mutation #", count);
+//     if(count >= 5) {
+//         observer.disconnect();
+//         console.log("Stopped observing after 5 mutations");
+//     }
+// });
+
+// observer.observe(document.body, { childList: true, subtree: true });
